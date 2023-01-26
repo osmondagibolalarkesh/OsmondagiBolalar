3351ecef63901cb87069c733ef82644721549035
A S L A N B E K <learneraslanbek1220@gmail.com>
2023-01-26 16:04:22 +0500

    Dadaxon proj

---
+++ Dadaxon/Program.cs
@@ -0,0 +1,49 @@
+﻿#region switch  2 - misol 
+// int k = int.Parse(Console.ReadLine());
+//switch (k)
+//{
+//case 1:
+//       Console.WriteLine(" yomon o'quvchi");
+//       break;
+//   case 2:
+//       Console.WriteLine(" qoniqarsiz o'quvchi");
+//       break;
+//   case 3:
+//       Console.WriteLine(" qoniqarli o'quvchi"); 
+//       break;
+//   case 4:
+//       Console.WriteLine(" yaxshi o'quvchi"); 
+//       break;
+//   case 5:
+//       Console.WriteLine(" alo o'quvchi");
+//       break;
+//   default:
+//   Console.WriteLine("Bugun ahvollar joyidami og'ayni???"); 
+//   break;
+//}
+#endregion
+#region switch 5 misol
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+switch (c)
+{
+	case 1:
+		Console.WriteLine(a + b);
+		break;
+	case 2:
+		Console.WriteLine(a - b);
+		break;
+	case 3:
+		Console.WriteLine(a / b);
+		break;
+	case 4:
+		Console.WriteLine(a * b);
+		break;
+	default:
+		Console.WriteLine(" I don't now ");
+		#endregion
+}
+
+
+
