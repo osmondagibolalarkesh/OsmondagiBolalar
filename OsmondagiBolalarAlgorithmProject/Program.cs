134e1406d12ea67a2ee6c939052ad90e875ac2c0
User <User@USER-1>
2023-01-26 15:07:48 +0500

    ternar operator

--- OsmondagiBolalarAlgorithmProject/Program.cs
+++ OsmondagiBolalarAlgorithmProject/Program.cs
@@ -1,2 +1,10 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿int ageIsDadaxon = int.Parse(Console.ReadLine());
+Console.WriteLine(ageIsDadaxon > 18 ? "balog'atga yetgan" : "balog'atga yetmagan");// ternar operator
+//if (ageIsDadaxon > 18)
+//{
+//    Console.WriteLine("balog'atga yetgan");
+//}
+//else
+//{
+//    Console.WriteLine("balog'atga yetmagan");
+//}
