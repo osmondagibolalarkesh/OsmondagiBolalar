f2d8d163429580c7691048a54609a324fa9c98dc
User <User@USER-1>
2023-01-26 15:19:28 +0500

    Case- 1

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,29 @@
+﻿//case 1- 
+Console.Write("Enter your week day"); int weekDay = int.Parse(Console.ReadLine());
+switch (weekDay)
+{
+	case 1:
+		Console.WriteLine("Dushanba");
+		break;
+    case 2:
+        Console.WriteLine("Seshanba");
+        break;
+    case 3:
+        Console.WriteLine("Chorshanba");
+        break;
+    case 4:
+        Console.WriteLine("Payshanba");
+        break;
+    case 5:
+        Console.WriteLine("Juma");
+        break;
+    case 6:
+        Console.WriteLine("Shanba");
+        break;
+    case 7:
+        Console.WriteLine("Yakshanba");
+        break;
+    default:
+        Console.WriteLine("\aThis is week day not found");
+        break;
+}
