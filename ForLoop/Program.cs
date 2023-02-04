89d45dc35c4dbdb35eb1685a2cafea4f141e0169
User <User@USER-1>
2023-02-04 17:15:44 +0500

    for example

--- ForLoop/Program.cs
+++ ForLoop/Program.cs
@@ -1,1 +1,77 @@
 ﻿
+//int N, Sum = 0;
+//Console.Write("N = "); N = int.Parse(Console.ReadLine());
+//for (int i = 0; i <= N; i++)
+//{
+//    //Sum += i;
+//    if (i % 2 == 0)
+//    {
+//        Sum = Sum + i;//Sum = 2
+//    }
+
+
+//}
+//Console.WriteLine(" Sum = " + Sum);
+
+//double  N, Sum = 0;
+//Console.Write("N = "); N = double.Parse(Console.ReadLine());
+//for (double i = 1; i <= N; i++)
+//{
+//    Console.WriteLine(i + " sonning kvadtrati = " + Math.Pow(i, 2));
+//}
+
+//int N, Sum = 0;
+//Console.Write("N = "); N = int.Parse(Console.ReadLine());
+
+//for (int i = 1; i <= N; i++)
+//{
+//    if ((i > 0 && i <= 10) && i % 2 == 1)
+//    {
+//        Console.WriteLine(i);
+//    }
+//    else if ((i > 10 && i <= N) && i % 2 == 0)
+//    {
+//        Console.WriteLine(i);
+//    }
+
+
+//}
+//int Sum = 0;
+//Console.Write("N = ");int  N = int.Parse(Console.ReadLine());
+//for (int i = 1;i <= N; i++)
+//{
+//    if (N / 2 > i) continue;
+//	{
+//        Sum += i;
+//    }
+
+//}
+//Console.WriteLine("Sum= " + Sum);
+//for -1
+//Console.Write("k = "); int k = Convert.ToInt32(Console.ReadLine()); 
+//Console.Write("n = "); int n = Convert.ToInt32(Console.ReadLine());
+//for (int i = 1; i <= n; i++)
+//{
+//    Console.WriteLine(k);
+//}
+//for- 4
+//int count = 0;
+// for - 7
+// for - 10
+//float S = 0;
+//Console.Write("n (n > 0) = "); float n = Convert.ToInt32(Console.ReadLine());
+////Console.Write("b = "); int b = Convert.ToInt32(Console.ReadLine());
+//for (float i = 1; i <= n; i++)
+//{
+//    S += 1 / i;
+//    //Console.WriteLine(i);
+//}
+//Console.WriteLine(S);
+//Console.WriteLine(count);
+
+int i = 1;
+for (; ; )
+{
+    Console.Write($"i = {i} ");
+    i++;
+}
